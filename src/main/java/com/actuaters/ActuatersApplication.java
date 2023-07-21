6c3be377fd3734403e0b9a07c12883463b3d8bd1
Rahulstar177 <rathodsachiit@gmaiil.com>
2023-07-21 20:27:42 +0530

    Demo of conflict

---
+++ src/main/java/com/actuaters/ActuatersApplication.java
@@ -0,0 +1,13 @@
+package com.actuaters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/actuaters/ActuatersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actuaters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
