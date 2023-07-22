52294a22265e48e1dd7aa46a7c6002d45b3ee125
Rahulstar177 <rathodsachiit@gmaiil.com>
2023-07-22 18:35:07 +0530

    this is the regular task

---
+++ src/main/java/com/actuaters/model/Home.java
@@ -0,0 +1,14 @@
+package com.actuaters.model;
+
+public class Home {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
